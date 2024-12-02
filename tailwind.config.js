0c07a5c5a1d75705b7ad95c8f0db83ab71c40730
penguinawesome1 <owen.h.colley@gmail.com>
2024-12-01 21:07:12 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	mode: "jit",
+	darkMode: "media",
+    content: [
+		"./index.html",
+    ],
+    theme: {
+		// extend: {
+		// 	colors: {
+		// 		primary: {
+		// 			DEFAULT: "white",
+		// 			dark: "black",
+		// 		},
+		// 		secondary: {
+		// 			DEFAULT: "green",
+		// 			dark: "blue",
+		// 		},
+		// 		text: {
+		// 			DEFAULT: "black",
+		// 			dark: "white",
+		// 		}
+		// 	},
+		// },
+    },
+    plugins: [],
+};
